14bf537eea0aac0a717b0668b14857a239cd67c6
SeeSharpCode <seesharpcode@gmail.com>
2017-09-28 16:37:27 -0500

    added Java and Spring Boot examples

---
+++ Java/sandbox/java/src/main/java/com/jbhunt/sandbox/ExceptionThrower.java
@@ -0,0 +1,7 @@
+package com.jbhunt.sandbox;
+
+public class ExceptionThrower {
+    public static void foo() throws Exception {
+        throw new Exception("Foo");
+    }
+}
---
+++ Java/sandbox/java/src/main/java/com/jbhunt/sandbox/MiddleMan.java
@@ -0,0 +1,7 @@
+package com.jbhunt.sandbox;
+
+public class MiddleMan {
+    public static void foo() throws Exception {
+        ExceptionThrower.foo();
+    }
+}
