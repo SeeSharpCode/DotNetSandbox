4d1ffae0f18413f6fb2db7d461a13a1b59824fec
SeeSharpCode <seesharpcode@gmail.com>
2017-10-12 12:18:02 -0500

    removing Java projects

--- Java/sandbox/java/src/test/java/com/jbhunt/sandbox/lang/Exceptions.java
+++
@@ -1,6 +0,0 @@
-package com.jbhunt.sandbox.lang;
-
-public class Exceptions {
-    // RuntimeExceptions are unchecked
-    // Checked exceptions inherit directly from Exception, e.g. IOException
-}
--- Java/sandbox/spring-boot/spring-boot-rest-jquery/src/main/java/com/sandbox/springbootrestjquery/Greeting.java
+++
@@ -1,9 +0,0 @@
-package com.sandbox.springbootrestjquery;
-
-import lombok.Data;
-
-@Data
-public class Greeting {
-    private final long id;
-    private final String content;
-}
